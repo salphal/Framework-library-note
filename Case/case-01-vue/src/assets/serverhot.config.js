219fbd61dc397d6c0020e1f683ee82cfeafd6781
Alpha-CL <xiaodao_92@yeah.net>
2020-07-17 14:54:45 +0800

    daily update

--- Case/case-01-vue/src/assets/serverhot.config.js
+++
@@ -1,7 +0,0 @@
-export const moduleHot = function () {
-
-    if (module.hot) {
-
-        module.hot.accept();
-    }
-};
--- Case/case-01-vue/src/index.js
+++
@@ -1,6 +0,0 @@
-import {moduleHot} from "./assets/serverhot.config.js";
-moduleHot();
-import "vue";
-
-import "./assets/js/vue-statement"
-import "./assets/js/vue-response"
